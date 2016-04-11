7de4b8d1a8ae1b013257a990f47646ab8ace5d24
Atousa Duprat <atousa.p@gmail.com>
2016-04-11 15:36:41 -0700

    changed the shareViewController

--- PhotogramSwift/ShareViewController.swift
+++ PhotogramSwift/ShareViewController.swift
@@ -6,30 +6,77 @@
 //  Copyright © 2016 Kevin Kim. All rights reserved.
 //
 
+
+//For Kevin: This object receives an image, annotates the image with a name and geotech info. and pass it to other object for further processing. It is augmented by a function named "didSharedImage". If you want to use this function you need to include the "ShareViewControllerDelegate" protocol in your object.
 import UIKit
+import Photos
+import MapKit
+
+//delegate protocol
+protocol ShareViewControllerDelegate: class {
+   func didSharedImage(sender: Image)
+}
 
 class ShareViewController: UIViewController {
+    //Image that passed from other VC
+    let recievedImage = UIImage()
+    let shareImage = Image()
+    
+    
+    //set up the delegate property
+    weak var delegate:ShareViewControllerDelegate?
 
+    //outlets
+    @IBOutlet weak var nameOfImage: UITextField!
+    @IBOutlet weak var imageToShare: UIImageView!
+    @IBOutlet weak var locationOfImage: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        shareImage.name = self.nameOfImage.text
+        shareImage.image = UIImagePNGRepresentation(recievedImage)
+        
+        //taking the geotech from the received image
+        let assetResult = PHAsset.fetchAssetsWithMediaType(.Image,
+                                                    options: nil)
+        print("Found \(assetResult.count) results")
+        let location: CLLocation = assetResult[0].location as CLLocation!
+        shareImage.latitude = location.coordinate.latitude
+        shareImage.longitude = location.coordinate.longitude
+        let geocoder = CLGeocoder()
+        
+        // convert coordinate to address and show it in the textField
+        geocoder.reverseGeocodeLocation(location) {
+            (placemarks: [CLPlacemark]?, error) -> Void in
+            let placemarks = placemarks! as [CLPlacemark]
+                var placemark = CLPlacemark()
+                for placemark in placemarks {
+                    self.locationOfImage.text = (placemark.locality)
+                }
+            }
     }
     
+        
+        // Kevin, When you want to call this methos you need to use this convention in swift :
+    //extension MasterViewController:  ShareViewControllerDelegate{
+        //func didSharedImage(sender: Image) {
+            // do stuff like saving the image
+      //  }
+   // }
+    
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onPressedShareButton(sender: AnyObject) {
+        
+        delegate?.didSharedImage(shareImage)
     }
-    */
+    
+    
+    //get GPS coordinate form Image
+    
+    
 
+   
 }

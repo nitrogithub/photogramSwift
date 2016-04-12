ecab5f2ae68ebccc1440757de30f2ebee7090a61
Atousa Duprat <atousa.p@gmail.com>
2016-04-12 09:51:14 -0700

    Regenerated model classes

--- Comment+CoreDataProperties.swift
+++ Comment+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Comment+CoreDataProperties.swift
 //  PhotogramSwift
 //
-//  Created by Atousa Duprat on 4/11/16.
+//  Created by Atousa Duprat on 4/12/16.
 //  Copyright © 2016 Kevin Kim. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu

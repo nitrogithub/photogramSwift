07970a8ac2f29bf7087cab39e66571bc1c65a42d
Atousa Duprat <atousa.p@gmail.com>
2016-04-12 10:34:19 -0700

    Connected the bottom of the spacer to an outlet that can be updated programmatically

--- PhotogramSwift/CommentVC.swift
+++ PhotogramSwift/CommentVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CommentVC: UIViewController {
 
+    @IBOutlet weak var spacerBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 

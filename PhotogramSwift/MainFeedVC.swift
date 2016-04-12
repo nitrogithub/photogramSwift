f17933447362f97fc2c7e647ec43ad27de5b9b4c
kk <github@mantaone.com>
2016-04-12 14:18:19 -0700

    changes to MainVC

--- PhotogramSwift/MainFeedVC.swift
+++ PhotogramSwift/MainFeedVC.swift
@@ -17,6 +17,8 @@ class MainFeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var moc = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
     
     var users = []
+    var images = []
+
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var homeButton: UIBarButtonItem!
     @IBOutlet weak var profileButton: UIBarButtonItem!
@@ -28,46 +30,82 @@ class MainFeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         self.moc = appDelegate.managedObjectContext
         
-//        testObject1.name = "Kevin"
-//        testObject1.image = "yosemite"
-//        testObject1.comments = ["wow thats great", "time to go try it out!","nice shot!"]
-//        
-//        testObject2.name = "Atousa"
-//        testObject2.image = "yosemite"
-//        testObject2.comments = ["interesting", "lame","nice!"]
-//        
-//        testObject3.name = "David"
-//        testObject3.image = "yosemite"
-//        testObject3.comments = ["hmmmmm...", "way to go","lolol"]
-//        testArray = [testObject1, testObject2, testObject3]
+     
         
         self.loadFromCoreData()
         if self.users.count == 0 {
             print("empty MOC, createInitialData")
             self.createInitialData()
+            self.loadFromCoreData()
         }
+        
+        self.tableView.reloadData()
         
     }
     
     
     
     func createInitialData() {
+        let nowTime = NSDate()
+        
+        print("\(nowTime)")
+        
         let user1 = NSEntityDescription.insertNewObjectForEntityForName("User", inManagedObjectContext: self.moc) as! User
         user1.realName = "Kevin Kim"
         user1.profileName = "KDawg"
         user1.gender = "Male"
-//        user1.image = 
+        
+        let user2 = NSEntityDescription.insertNewObjectForEntityForName("User", inManagedObjectContext: self.moc) as! User
+        user2.realName = "Atousa D"
+        user2.profileName = "ADawg"
+        user2.gender = "Female"
+        
+        let user3 = NSEntityDescription.insertNewObjectForEntityForName("User", inManagedObjectContext: self.moc) as! User
+        user3.realName = "David I"
+        user3.profileName = "DDawg"
+        user3.gender = "Male"
+        
+        let image1 = NSEntityDescription.insertNewObjectForEntityForName("Image", inManagedObjectContext: self.moc) as! Image
+        image1.image = UIImagePNGRepresentation(UIImage.init(named: "yosemite")!)
+//        image1.timeStamp =
+        image1.user? = user1
+        
+        let image2 = NSEntityDescription.insertNewObjectForEntityForName("Image", inManagedObjectContext: self.moc) as! Image
+        image2.image = UIImagePNGRepresentation(UIImage.init(named: "squareImage1")!)
+        //        image1.timeStamp =
+        image2.user? = user2
+        
+        let image3 = NSEntityDescription.insertNewObjectForEntityForName("Image", inManagedObjectContext: self.moc) as! Image
+        image3.image = UIImagePNGRepresentation(UIImage.init(named: "squareImage2")!)
+        //        image1.timeStamp =
+        image3.user? = user2
+        
+        
+        //temporarily disabled saving data
         self.saveData()
     }
     
     
-    func loadFromCoreData (){
+    func loadFromCoreData(){
         //        MOC and fetching data
-        let fetchRequest = NSFetchRequest(entityName: "User")
+        let fetchRequest1 = NSFetchRequest(entityName: "User")
         do {
-            let results = try moc.executeFetchRequest(fetchRequest)
+            let results = try moc.executeFetchRequest(fetchRequest1)
             //            creaturesMOC = results as! [NSManagedObject]
             self.users = results as! [NSMutableArray]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+        
+        
+        
+        
+        
+        let fetchRequest2 = NSFetchRequest(entityName: "Image")
+        do {
+            let results = try moc.executeFetchRequest(fetchRequest2)
+            //            creaturesMOC = results as! [NSManagedObject]
+            self.images = results as! [NSMutableArray]
         } catch let error as NSError {
             print("Could not fetch \(error), \(error.userInfo)")
         }
@@ -85,10 +123,17 @@ class MainFeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("feedCell", forIndexPath: indexPath) as! FeedTableViewCell
-        let userTemp = self.users[indexPath.row] as! User
-        cell.usernameLabel.text = userTemp.profileName
-        print("\(userTemp.profileName)")
-//        cell.profileImage.image = 
+//        let userTemp = self.users[indexPath.row] as! User
+//        let imageTemp = self.images[indexPath.row] as! Image
+//        print("\(imageTemp)")
+//        cell.usernameLabel.text = imageTemp.user?.profileName
+//        cell.mainImageView.image = UIImage(data: imageTemp.image!)
+//
+        cell.imageCell = self.images[indexPath.row] as? Image
+        
+        
+//        print("\(userTemp.profileName)")
+//        cell.profileImage.image =
 //        cell.timeUploadedLabel.text =
 //        cell.mainImageView.image = 
         
@@ -101,11 +146,11 @@ class MainFeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     }
 
     func commentSeguePlease() {
-        self.performSegueWithIdentifier("commentSegue", sender: nil)
+        self.performSegueWithIdentifier("segue1", sender: nil)
     }
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.users.count
+        return self.images.count
     }
 }
